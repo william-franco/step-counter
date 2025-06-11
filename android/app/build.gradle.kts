fb0a45a75d17d50da6899665f5a04e109af61cbd
William Franco <williamff@outlook.com>
2025-06-11 18:17:31 -0300

    :arrow_up: Updated dependencies.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "br.com.williamfranco.step_counter"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
